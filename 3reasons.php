86de1bf253f9aa42ce700961ed6470ce53ea0c61
Moazam05 <salmanmoazam08@gmail.com>
2024-02-28 20:25:27 +0500

    last section

--- 3reasons.php
+++ 3reasons.php
@@ -204,6 +204,196 @@ include "header.php";
     </div>
 </section>
 
+<!-- 8th Section -->
+<section class="appC investMarket last-3Reason">
+    <!--  -->
+    <div class="investBggray">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 ">
+                    <div class="f_div">
+                        <h2 class="f_div_title">
+                            <object data="./assets/img/clipboard-text.svg" type="image/svg+xml"></object>
+                            SEC Filings
+                        </h2>
+                        <a href="https://www.sec.gov/edgar/browse/?CIK=1371128" target="_blank">
+                            See our fillings at SEC.gov
+                        </a>
+                        <hr>
+                        <h2 class="f_div_title">
+                            <object data="./assets/img/briefcase.svg" type="image/svg+xml"></object>
+                            Transfer Agent
+                        </h2>
+                        <br>
+                        <h3>
+                            Worldwide Stock Transfer, LLC
+                        </h3>
+                        <br>
+                        <p>
+                            One University Plaza Suite 505
+                        </p>
+                        <p>
+                            Hackensack, Nj 07601
+                        </p>
+                        <p>Tel: (201) 820-2008 </p>
+                        <p>URL: www.worldwidestocktransfer.com</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 ">
+                    <div class="s_div">
+
+                        <h2 class="f_div_title">
+                            <object data="./assets/img/message-question.svg" type="image/svg+xml"></object>
+                            Investor FAQ
+                        </h2>
+                        <div class="faqs">
+                            <div class="accordion" id="accordionExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingOne">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                            What is the legal status of the company and fiscal year end?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            <p>NewHydrogen, Inc. is a Nevada corporation with a fiscal year end of December 31st.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingTwo">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                            Who is your transfer agent? Who should I contact regarding my stock certificate(s)?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            <p>Worldwide Stock Transfer, LLC <br>
+
+                                                One University PlazaSuite 505<br>
+
+                                                Hackensack, NJ 07601<br>
+
+                                                Tel (201) 820-2008<br>
+
+                                                URL : www.worldwidestocktransfer.com<br>
+
+                                                Our transfer agent can help with a variety of shareholder-related services. If you own shares through a brokerage firm account, you should contact your broker first for information about your account and requests for a stock certificate.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingThree">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                            I purchased stock from the company in a private placement, when do I receive my stock certificate?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            <p>We automatically enroll investors in a new, convenient service offered by our transfer agent called Book Entry form. With Book Entry, our transfer agent holds your shares electronically, hence making safekeeping and transactions quicker and easier. Instead of sending you a physical stock certificate, they generate and mail you an account statement, which is the proof of your ownership of the shares. If you want a physical stock certificate, please contact the transfer agent and request one.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingFour">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                            How do I sell the shares that I purchased from the company?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            <p>All shares purchased directly from the company are restricted shares, as defined by the SEC. To sell your shares, you must apply to have the restriction removed. If you have held your shares for more than 6 months or past your lock-up period, whichever is later, you may be eligible to remove the restrictive legend on your certificate under SEC Rule 144 with a proper legal opinion letter, and then sell those shares in the public market. Please contact your broker or the transfer agent to begin the process of removing the restrictive legend. They will request additional paperwork from you. The Company does not accept legend removal requests directly from shareholders.Please note that the legend removal process may take up to 4 weeks to complete through your broker.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingFive">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                            What is a Rule 144 legal opinion letter and how do I get one?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            <p>When selling shares from physical certificates bearing a restrictive legend or Computershare Restricted Transaction Statements, a legal opinion from the company’s SEC counsel is required. Your broker should be able to handle everything for you. The company’s SEC counsel is: <br>
+
+                                                <strong>Sichenzia Ross Ference LLP</strong> <br>
+
+                                                David Manno, Esq. <br>
+
+                                                1185 Avenue of the Americas, 37th Floor <br>
+
+                                                New York, NY 10036 <br>
+
+                                                F: 212-930-9725 <br>
+
+                                                Email: dmanno@srf.law
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingFive">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                                            When and how are news releases issued by the company?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            <p>News is generally released by the company through newswire services such as BusinessWire and MarketWire. In the interest of fair disclosure to the general public, we refrain from answering questions from individual investors regarding the company’s development progress.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingFive">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                                            How can I receive on-going information about NewHydrogen?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            <p>The best way to receive on-going information about NewHydrogen is by subscribing to our newsletter.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="investorForm">
+        <div class="container">
+            <h4>
+                Investor Relations
+            </h4>
+            <div class="relation">
+                <div class="relation-form">
+                    <form action="./php/investorForm.php" method="POST">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" placeholder="First Name" name="fname" id="fname" required>
+                            </div>
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" placeholder="Last Name" name="lname" id="lname" required>
+                            </div>
+                        </div>
+
+                        <input type="email" class="form-control" placeholder="Email" name="email" id="email" required>
+                        <input type="text" class="form-control" placeholder="Subject" name="subject" id="subject" required>
+                        <textarea name="message" id="message" cols="30" rows="5" class="form-control" placeholder="Message" required></textarea>
+                        <input type="hidden" name="recaptcha_response" id="recaptchaResponse">
+                        <div class="contact-form-btn">
+                            <button type="submit" class="theme-btn">Send Message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 
 
 <?php include "footer.php"; ?>
